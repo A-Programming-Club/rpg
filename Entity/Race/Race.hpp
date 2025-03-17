30e4da064ae912df63e1cfd5b73a4d900350f680
BamyLamy <easportplaythegame23@gmail.com>
2025-03-17 21:01:01 +0000

    Did race code and will work more on entity. Probably needs more extending. If not then I will move to Items.

--- Entity/Race/Race.hpp
+++ Entity/Race/Race.hpp
@@ -5,13 +5,27 @@
 class Race
 {
 public:
+   Race(const std::string &name,const double BASE_HEALTH, const double BASE_AGILITY, const double BASE_STRENGTH);
+   void base_stats(Race){
+    std::cout << "Race =" << name << std::endl;
+    std::cout << "Health =" << BASE_HEALTH << std::endl;
+    std::cout << "Agility =" << BASE_AGILITY << std::endl;
+    std::cout << "BASE_STRENGTH" << BASE_STRENGTH << std::endl;
+   }
+
+private:
     std::string name;
-    const double BASE_HEALTH;
+    const double BASE_HEALTH;;
     const double BASE_AGILITY;
     const double BASE_STRENGTH;
-
-    Race(const std::string &name, const double health, const double agility, const double strength);
-private:
+    const double BASE_DEXTERITY;
+    const double BASE_MANA;
+    const double BASE_STAMINA;
+    const double BASE_CONSTITUTION;
+    const double BASE_INTELLIGENCE;
+    const double BASE_WISDOM;
+    const double BASE_CHARISMA;
+    const double BASE_HITPOINTS;
 };
 
 #endif
